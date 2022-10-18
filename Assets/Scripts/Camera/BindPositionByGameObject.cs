2a0046eef3a1a7096de5c4a90b66bdef45f19aa4
lyp <970387216@qq.com>
2022-10-18 08:52:44 +0800

    添加相机跟随移动脚本

---
+++ Assets/Scripts/Camera/BindPositionByGameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Camera
+{
+    public class BindPositionByGameObject : MonoBehaviour
+    {
+        public  GameObject target;
+        private GameObject current;
+        private Vector3    offset;
+        private void Awake()
+        {
+            if (!target)
+            {
+                throw new Exception("需要绑定游戏对象目标");
+            }
+
+            // check current is camera
+            current = gameObject;
+            if (current.GetComponent<UnityEngine.Camera>() == null)
+            {
+                throw new Exception("当前对象不是摄像机");
+            }
+            
+            offset = current.transform.position - target.transform.position;
+        }
+
+        private void Update()
+        {
+            // update position
+            current.transform.position = target.transform.position + offset;
+        }
+    }
+}
