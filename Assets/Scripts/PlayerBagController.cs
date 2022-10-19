6a57f4b28b33025605a59d3252d2246216688d93
lyp <970387216@qq.com>
2022-10-19 14:12:33 +0800

    激活 Player Action Map

--- Assets/Scripts/PlayerBagController.cs
+++ Assets/Scripts/PlayerBagController.cs
@@ -1,40 +1,51 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
-    public class PlayerBagController : MonoBehaviour
+public class PlayerBagController : MonoBehaviour
+{
+    private PlayerInputActions _actions;
+    private GameObject         _player;
+
+    public GameObject[] bag;
+    public int          bagIndex = 0;
+    public int          bagSize  = 8;
+
+    private void Start()
     {
-        private PlayerInputActions _actions;
-        private GameObject _player;
-
-        public GameObject[] bag;
-        public int          bagIndex = 0;
-        public int          bagSize  = 8;
-
-        private void Awake()
+        if (!CompareTag("Player"))
         {
-            if (!CompareTag("Player"))
-            {
-                throw new Exception("PlayerBagController 必须绑定到 tag 'Player' 对象上");
-            }
-
-            _player  =   this.gameObject;
-            bag      =   new GameObject[8];
-            
-            _actions = new PlayerInputActions();
-
-            _actions.Player.DropItem.performed += DropItem;                                                 // 丢弃物品
-            _actions.Player.UseItem.performed  += UseItem;                                                  // 使用物品
-            _actions.Player.NextItem.performed += context => bagIndex = (bagIndex     + 1)       % bagSize; // 下一个物品
-            _actions.Player.PostItem.performed += context => bagIndex = (bagIndex - 1 + bagSize) % bagSize; // 上一个物品
+            throw new Exception("PlayerBagController 必须绑定到 tag 'Player' 对象上");
         }
 
-        public void DropItem(InputAction.CallbackContext ctx)
-        {
-        }
+        _player = this.gameObject;
+        bag     = new GameObject[8];
 
-        public void UseItem(InputAction.CallbackContext ctx)
+        _actions = new PlayerInputActions();
+
+        _actions.Player.DropItem.performed += DropItem; // 丢弃物品
+        _actions.Player.UseItem.performed  += UseItem;  // 使用物品
+        _actions.Player.NextItem.performed += context =>
         {
-        }
+            Debug.Log(context.ReadValueAsButton());
+            bagIndex = (bagIndex + 1) % bagSize;
+        }; // 下一个物品
+        _actions.Player.PostItem.performed += context =>
+        {
+            Debug.Log(context.ReadValueAsButton());
+            bagIndex = (bagIndex - 1 + bagSize) % bagSize;
+        }; // 上一个物品
+
+        _actions.Player.Enable();
     }
+
+    public void DropItem(InputAction.CallbackContext ctx)
+    {
+        Debug.Log(ctx.ReadValueAsButton());
+    }
+
+    public void UseItem(InputAction.CallbackContext ctx)
+    {
+        Debug.Log(ctx.ReadValueAsButton());
+    }
+}
