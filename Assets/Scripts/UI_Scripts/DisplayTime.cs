299d86e6d6e52932a0263d14bd3d6ad49c33232e
lyp <970387216@qq.com>
2022-11-04 14:05:02 +0800

    测试动态帧率

---
+++ Assets/Scripts/UI_Scripts/DisplayTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI_Scripts
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class DisplayTime : MonoBehaviour
+    {
+        [SerializeField] public TextMeshProUGUI display;
+
+        private void Start()
+        {
+            display = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            display.SetText(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+        }
+    }
+}
--- Assets/Scripts/UI_Scripts/TestUiEvent.cs
+++ Assets/Scripts/UI_Scripts/TestUiEvent.cs
@@ -1,13 +1,69 @@
 ﻿using System;
 using Sirenix.OdinInspector;
-using Unity.VisualScripting;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace UI_Scripts
 {
+    [RequireComponent(typeof(TextMeshProUGUI))]
     public class TestUiEvent : MonoBehaviour
     {
+        [SerializeField, LabelText("当前对象"),] public TextMeshProUGUI display;
+
+        [SerializeField, Range(0.1f, 2f), LabelText("FPS采样时间"),]
+        public float sampleDuration = 0.25f;
+
+        [LabelText("失去焦点降帧率")] public bool isFocusChangeFrameRate = true;
+
+        private long  frames;
+        private float duration, best = float.MaxValue, worst = float.MinValue;
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            if (isFocusChangeFrameRate)
+            {
+                if (hasFocus)
+                {
+                    Application.targetFrameRate = -1;
+                }
+                else
+                {
+                    Application.targetFrameRate = 1;
+                }
+            }
+        }
+
+        private void Start()
+        {
+            display = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            float frameDuration = Time.unscaledDeltaTime;
+            frames   += 1;
+            duration += frameDuration;
+            best     =  math.min(best, frameDuration);
+            worst    =  math.max(worst, frameDuration);
+
+            if (duration >= sampleDuration)
+            {
+                display.SetText(
+                    $"FPS\n"                     +
+                    $"{1f     / best:0.0}\n"     +
+                    $"{frames / duration:0.0}\n" +
+                    $"{1f     / worst:0.0}");
+                frames   = 0;
+                duration = 0;
+                best     = float.MaxValue;
+                worst    = float.MinValue;
+            }
+        }
+
+
         [LabelText("是否打印资源路径")] public bool isPrintPath = true;
+
         private void Awake()
         {
             if (isPrintPath)
