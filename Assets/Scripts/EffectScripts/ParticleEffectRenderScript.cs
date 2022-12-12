df9eafd926ee00f39d5de23e6d870664db0eb0d8
lyp <970387216@qq.com>
2022-12-12 16:40:19 +0800

    加一个粒子系统测试

---
+++ Assets/Scripts/EffectScripts/ParticleEffectRenderScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EffectScripts
+{
+    public class ParticleEffectRenderScript : MonoBehaviour
+    {
+        public Vector3 particlePositionCenter;
+        public float   particleRange;
+
+        private List<Vector4> customData = new List<Vector4>();
+
+        private void Awake()
+        {
+            Collider2D component = this.GetComponent<Collider2D>();
+            particlePositionCenter = component.bounds.center;
+
+            var particleSystem = this.GetComponent<ParticleSystem>();
+            particleSystem.GetCustomParticleData(customData, ParticleSystemCustomData.Custom1);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(particlePositionCenter, particleRange);
+        }
+    }
+}
