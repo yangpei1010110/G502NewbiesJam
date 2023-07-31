4c3787c16357a6a7971635c22be7e7148a617a50
PigeonDuo <jreeydy@gmail.com>
2023-07-31 17:59:24 +0800

    更新——更新 角色升级 extend，demo

---
+++ Assets/ProgressionSystem/Scripts/Extensions/MultipliedDamageOnTouch.cs
@@ -0,0 +1,45 @@
+﻿using MoreMountains.Tools;
+using MoreMountains.TopDownEngine;
+using ProgressionSystem.Scripts.Variables;
+using UnityEngine;
+
+namespace ProgressionSystem.Scripts.Extensions
+{
+    public class MultipliedDamageOnTouch : DamageOnTouch
+    {
+        [SerializeField] private FloatVariable DamageMultiplier;
+
+        protected override void OnCollideWithDamageable(Health health)
+        {
+            if (!health.CanTakeDamageThisFrame())
+            {
+                return;
+            }
+			
+            // if what we're colliding with is a TopDownController, we apply a knockback force
+            _colliderTopDownController = health.gameObject.MMGetComponentNoAlloc<TopDownController>();
+
+            HitDamageableFeedback?.PlayFeedbacks(transform.position);
+
+            // we apply the damage to the thing we've collided with
+            var randomDamage = DamageMultiplier.Value * Random.Range(MinDamageCaused, Mathf.Max(MaxDamageCaused, MinDamageCaused));
+			
+            ApplyKnockback(randomDamage, TypedDamages);
+
+            if (RepeatDamageOverTime)
+            {
+                _colliderHealth.DamageOverTime(randomDamage, gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection, TypedDamages, AmountOfRepeats, DurationBetweenRepeats, DamageOverTimeInterruptible, RepeatedDamageType);	
+            }
+            else
+            {
+                _colliderHealth.Damage(randomDamage, gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection, TypedDamages);	
+            }
+
+            // we apply self damage
+            if (DamageTakenEveryTime + DamageTakenDamageable > 0 && !_colliderHealth.PreventTakeSelfDamage)
+            {
+                SelfDamage(DamageTakenEveryTime + DamageTakenDamageable);
+            }
+        }
+    }
+}
---
+++ Assets/ProgressionSystem/Scripts/UI/ExperienceProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using MoreMountains.Tools;
+using ProgressionSystem.Scripts.Core;
+using UnityEngine;
+
+namespace ProgressionSystem.Scripts.UI
+{
+    public class ExperienceProgressBar : MonoBehaviour
+    {
+        [SerializeField] private Progression Progression;
+        private MMProgressBar _bar;
+        
+        private void Awake() { _bar = GetComponent<MMProgressBar>(); }
+
+        private void UpdateBar()
+        {
+            if (Progression.NextLevelExperience > 0)
+                _bar.UpdateBar(Progression.LevelExperience, 0, Progression.NextLevelExperience);
+            else
+                _bar.SetBar01(1);
+        }
+        private void OnEnable()
+        {
+            _bar.SetBar(Progression.LevelExperience, 0, Progression.NextLevelExperience);
+            Progression.Progressed += UpdateBar;
+        }
+        private void OnDisable() { Progression.Progressed -= UpdateBar; }
+    }
+}
