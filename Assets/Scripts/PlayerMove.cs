081aa85d182dceb429b57db27d99985612df4fb7
yangpei1010110 <970387216@qq.com>
2023-09-05 07:18:05 +0800

    修改角色控制器状态检测逻辑

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
@@ -10,17 +11,21 @@ public class PlayerMove : MonoBehaviour
         OnAir,
     }
 
-    public  State       PlayerState  = State.OnGround;
-    public  float       JumpVelocity = 20f;
-    public  float       MoveForce    = 20f;
-    public  float       MaxForce     = 20f;
-    public  float       MaxSpeed     = 20f;
-    private Rigidbody2D rb;
+    public  State            PlayerState  = State.OnGround;
+    public  float            JumpVelocity = 20f;
+    public  float            MoveForce    = 20f;
+    public  float            MaxForce     = 20f;
+    public  float            MaxSpeed     = 20f;
+    private Rigidbody2D      rb;
+    private CircleCollider2D circleCollider2D;
+    private Collider2D[]     results = new Collider2D[25];
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        circleCollider2D = GetComponent<CircleCollider2D>();
         StartCoroutine(StopTopDownController2D());
     }
 
@@ -41,6 +46,7 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        StateChange();
         if (Input.GetKey(KeyCode.Space) && PlayerState == State.OnGround)
         {
             var velocity = rb.velocity;
@@ -48,30 +54,40 @@ public class PlayerMove : MonoBehaviour
             rb.velocity = velocity;
         }
 
+        var force = Vector2.zero;
         if (PlayerState == State.OnGround)
         {
-            var force = Vector2.zero;
             force += new Vector2(Input.GetAxis("Horizontal"), 0) * MoveForce;
-            force = Vector2.ClampMagnitude(force, MaxForce);
-            rb.AddForce(force);
         }
+        else
+        {
+            force += new Vector2(Input.GetAxis("Horizontal"), 0) * (MoveForce * 0.1f);
+        }
+
+        force = Vector2.ClampMagnitude(force, MaxForce);
+        rb.AddForce(force);
 
         rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxSpeed);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void StateChange()
     {
-        if (other.gameObject.CompareTag("Mine"))
+        var castCount = Physics2D.OverlapCircleNonAlloc((Vector2)transform.position + circleCollider2D.offset + Vector2.down * 0.1f, circleCollider2D.radius, results);
+        for (int i = 0; i < castCount; i++)
         {
-            PlayerState = State.OnGround;
-        }
-    }
+            var result = results[i];
+            if (result == circleCollider2D)
+            {
+                continue;
+            }
 
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Mine"))
-        {
-            PlayerState = State.OnAir;
+            if (result.CompareTag("Mine"))
+            {
+                PlayerState = State.OnGround;
+                return;
+            }
         }
+
+        PlayerState = State.OnAir;
     }
 }
